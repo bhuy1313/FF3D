f302f990d22688754a0d081743e121c5f0ba2168
HieuBui-MI <bhuy1313@gmail.com>
2026-04-08 04:59:05 +0700

    Updt

--- Assets/ThirdParty/Samples/Scriptable Render Pipeline Core/17.3.0/Common/Scripts/Editor/SamplesShowcaseEditorWindow.cs
+++ Assets/ThirdParty/Samples/Scriptable Render Pipeline Core/17.3.0/Common/Scripts/Editor/SamplesShowcaseEditorWindow.cs
@@ -19,7 +19,7 @@ public class SamplesWindow : EditorWindow
 
     static void SceneOpened(Scene scene, OpenSceneMode openSceneMode)
     {
-        var currentShowcase = (SamplesShowcase)FindFirstObjectByType(typeof(SamplesShowcase));
+        var currentShowcase = (SamplesShowcase)Object.FindAnyObjectByType(typeof(SamplesShowcase));
         if(currentShowcase != null)
             Selection.activeGameObject = currentShowcase.gameObject;
     }
